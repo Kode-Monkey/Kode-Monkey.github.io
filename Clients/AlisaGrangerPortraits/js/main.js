7fba5a75168f8bea31cba37b4dd8aa24bf945e68
Kfn8dkodemonkey <kaffeinatedkodemonkey@gmail.com>
2017-11-02 09:05:10 -0600

    update go live on alisas site

--- Clients/AlisaGrangerPortraits/js/main.js
+++ Clients/AlisaGrangerPortraits/js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "November 1, 2017 18:03:26",
+	date: "November 15, 2017 18:03:26",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
